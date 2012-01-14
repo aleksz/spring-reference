2bf44b3c362fe46b5e1e2b2822b0945fb75ec6c0
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2012-01-14 21:34:35 +0200

    Added binding error unit test for CreateOrderController.create

---
+++ src/test/java/com/gmail/at/zhuikov/aleksandr/servlet/controllers/CreateOrderControllerTest.java
@@ -0,0 +1,40 @@
+package com.gmail.at.zhuikov.aleksandr.servlet.controllers;
+
+import static junit.framework.Assert.fail;
+import static org.springframework.test.web.ModelAndViewAssert.assertModelAttributeAvailable;
+import static org.springframework.test.web.ModelAndViewAssert.assertViewName;
+
+import org.junit.Test;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndViewDefiningException;
+
+import com.gmail.at.zhuikov.aleksandr.servlet.domain.Order;
+
+public class CreateOrderControllerTest {
+
+	private CreateOrderController controller = new CreateOrderController();
+	
+	@Test
+	public void createThrowsExceptionInCaseOfBindingError() {
+		
+		Order order = new Order("x");
+		BindingResult errors = new BindException(order, "order");
+		errors.reject("some error");
+		Model model = new ExtendedModelMap();
+		model.addAttribute(order);
+		
+		try {
+			controller.create(order, errors, model);
+		} catch (ModelAndViewDefiningException e) {
+			assertViewName(e.getModelAndView(), "addOrder");
+			assertModelAttributeAvailable(e.getModelAndView(), "order");
+			return;
+		}
+		
+		fail("Shoudl throw exception");
+	}
+
+}
