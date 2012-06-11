b8311fc28aa0427fe646f549992b511c2afe722e
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2012-06-11 17:48:29 +0300

    reading sauce credentials from evn variables

--- src/ui-test/java/com/gmail/at/zhuikov/aleksandr/it/AbstractWebDriverTest.java
+++ src/ui-test/java/com/gmail/at/zhuikov/aleksandr/it/AbstractWebDriverTest.java
@@ -26,34 +26,6 @@ public abstract class AbstractWebDriverTest {
 	private final String relativeUrl;
 	@Rule public TestName name = new TestName();
 	
-//	@Rule public TestWatcher watcher = new TestWatcher() {
-//
-//		@Override
-//		protected void succeeded(Description description) {
-//			if (driver instanceof SauceOnDemandSelenium) {
-//				try {
-//					((SauceOnDemandSelenium) driver).jobPassed();
-//				} catch (IOException e1) {
-//					throw new RuntimeException(e1);
-//				}
-//			}
-//			driver.quit();
-//		}
-//
-//		@Override
-//		protected void failed(Throwable e, Description description) {
-//			if (driver instanceof SauceOnDemandSelenium) {
-//				try {
-//					((SauceOnDemandSelenium) driver).jobFailed();
-//				} catch (IOException e1) {
-//					throw new RuntimeException(e1);
-//				}
-//			}
-//			driver.quit();
-//		}
-//		
-//	};
-	
 	public AbstractWebDriverTest(String relativeUrl) {
 		this.relativeUrl = relativeUrl;
 	}
@@ -62,17 +34,12 @@ public abstract class AbstractWebDriverTest {
 		
 		String seleniumDriverUri = System.getenv("SELENIUM_DRIVER");
 		seleniumDriverUri += "&job-name=" + getClass().getName() + "." + name.getMethodName();
+		seleniumDriverUri += "&username=" + System.getenv("SAUCE_ONDEMAND_USERNAME");
+		seleniumDriverUri += "&access-key=" + System.getenv("SAUCE_ONDEMAND_ACCESS_KEY");
 		
 		WebDriver driver = SeleniumFactory.createWebDriver(seleniumDriverUri,
 				System.getenv("SELENIUM_STARTING_URL"));
 
-//		if (driver instanceof SauceOnDemandSelenium) {
-//			try {
-//				((SauceOnDemandSelenium) driver).setBuildNumber(System.getenv("BUILD_NUMBER"));
-//			} catch (IOException e) {
-//				throw new RuntimeException(e);
-//			}
-//		}
 		return driver;
 //		DesiredCapabilities capabillities = DesiredCapabilities.firefox();
 //		capabillities.setCapability("version", "5");
