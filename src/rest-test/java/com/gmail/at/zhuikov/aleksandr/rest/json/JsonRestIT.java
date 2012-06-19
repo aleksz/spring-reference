c051f60f4d9d4aa46a4b5f164773da4c46a3087b
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2012-06-19 09:38:46 +0300

    Resurrecting rest tests

---
+++ src/rest-test/java/com/gmail/at/zhuikov/aleksandr/rest/json/JsonRestIT.java
@@ -0,0 +1,194 @@
+package com.gmail.at.zhuikov.aleksandr.rest.json;
+
+import static junit.framework.Assert.assertEquals;
+import static org.apache.http.auth.AuthScope.ANY_REALM;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.springframework.data.domain.Sort.Direction.DESC;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.validation.FieldError;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+import com.gmail.at.zhuikov.aleksandr.rest.xml.MyResponseErrorHandler;
+import com.gmail.at.zhuikov.aleksandr.rest.xml.MyResponseErrorHandler.MyHttpStatusCodeException;
+import com.gmail.at.zhuikov.aleksandr.root.domain.Item;
+import com.gmail.at.zhuikov.aleksandr.root.domain.Order;
+import com.gmail.at.zhuikov.aleksandr.root.domain.xml.XmlFriendlyPage;
+import com.gmail.at.zhuikov.aleksandr.servlet.MappingJackson2HttpMessageConverter;
+
+public class JsonRestIT {
+
+	private static final Logger LOG = LoggerFactory.getLogger(JsonRestIT.class);
+	private static final String SERVER = "http://localhost:8080/spring-reference";
+	
+	private RestTemplate restTemplate;
+	
+	@Before
+	public void createRestTemplate() {
+		restTemplate = new RestTemplate(createHttpClientFactory("localhost", 8080));
+		final ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.setAnnotationIntrospector(new JaxbAnnotationIntrospector());
+		objectMapper.enableDefaultTyping(DefaultTyping.JAVA_LANG_OBJECT, As.PROPERTY);
+		List<HttpMessageConverter<?>> converters = new ArrayList<HttpMessageConverter<?>>();
+		converters.add(new StringHttpMessageConverter());
+		converters.add(new MappingJackson2HttpMessageConverter() {{
+			setObjectMapper(objectMapper);
+		}});
+		restTemplate.setMessageConverters(converters);
+	}
+	
+	private HttpComponentsClientHttpRequestFactory createHttpClientFactory(
+			String serverName, int serverPort) {
+
+		DefaultHttpClient client = new DefaultHttpClient();
+		client.getCredentialsProvider().setCredentials(
+		        new AuthScope(serverName, serverPort, ANY_REALM), 
+		        new UsernamePasswordCredentials("restuser", "ohW559f5"));
+
+		return new HttpComponentsClientHttpRequestFactory(client);
+	}
+	
+	@Test
+	public void listOrders() throws JsonParseException, IOException {
+		String result = restTemplate.getForObject(SERVER + "/orders", String.class);
+		LOG.info(result.toString());
+		new ObjectMapper().readValue(result, JsonNode.class);
+	}
+	
+	@Test
+	public void listOrdersReturnsPage() throws JsonParseException, IOException {
+		Order order = new Order("super customer");
+		order.setEmail("customer@example.com");
+		restTemplate.postForLocation(SERVER + "/orders", order);
+		Page<Order> result = restTemplate.getForObject(SERVER + "/orders", XmlFriendlyPage.class);
+		LOG.info(result.toString());
+		assertEquals(0, result.getNumber());
+		assertEquals(20, result.getSize());
+		assertFalse(result.getContent().isEmpty());
+		assertTrue(result.getNumberOfElements() > 0);
+		assertTrue(result.getTotalElements() >= 1);
+		assertTrue(result.getTotalPages() >= 1);
+		assertNotNull(result.getSort());
+		assertEquals(DESC, result.getSort().getOrderFor("date").getDirection());
+	}
+	
+	@Test
+	public void createOrderAndPrintJson() {
+		Order order = new Order("super customer");
+		order.setEmail("customer@example.com");
+		System.out.println(restTemplate.postForObject(SERVER + "/orders", order, String.class));
+	}
+	
+	@Test
+	public void createOrder() {
+		Order order = new Order("super customer");
+		order.setEmail("customer@example.com");
+		Order response = restTemplate.postForObject(SERVER + "/orders", order, Order.class);
+		assertNotNull(response);
+		assertEquals(order, response);
+		assertNotNull(response.getId());
+	}
+	
+	@Test
+	public void createOrderWithItem() {
+		Order order = new Order("super customer");
+		order.setEmail("customer@example.com");
+		new Item(order, "x", 1).setQuantity(1);
+		Order response = restTemplate.postForObject(SERVER + "/orders", order, Order.class);
+		assertNotNull(response);
+		assertEquals(order, response);
+		assertNotNull(response.getId());
+	}
+	
+	@Test
+	public void createOrderAndGetLocation() {
+		Order order = new Order("super customer");
+		order.setEmail("customer@example.com");
+		LOG.info(restTemplate.postForLocation(SERVER + "/orders", order).toString());
+	}
+	
+	@Test
+	public void createWrongOrder() throws JsonParseException, JsonMappingException, IOException {
+		Order request = new Order("2444");
+		request.setEmail("customer@example.com");
+		try {
+			restTemplate.postForObject(SERVER + "/orders", request, Order.class);
+		} catch (HttpClientErrorException e) {
+			LOG.info(e.getResponseBodyAsString());
+			assertEquals(BAD_REQUEST, e.getStatusCode());
+			return;
+		}
+		fail("Should throw exception");
+	}
+	
+	@Test
+	public void createWrongOrderWithCustomErrorHandler() throws JsonParseException, JsonMappingException, IOException {
+		
+		restTemplate.setErrorHandler(new MyResponseErrorHandler(
+				restTemplate.getMessageConverters()));
+
+		
+		Order request = new Order("2444");
+		request.setEmail("customer@example.com");
+		try {
+			restTemplate.postForObject(SERVER + "/orders", request, Order.class);
+		} catch (MyHttpStatusCodeException e) {
+			assertEquals(request, e.getErrorBody().getTarget());
+			assertFalse(e.getErrorBody().getErrors().isEmpty());
+			assertTrue(e.getErrorBody().getErrors().get(0) instanceof FieldError);
+			FieldError error = (FieldError) e.getErrorBody().getErrors().get(0);
+			assertEquals("order", error.getObjectName());
+			assertEquals("customer", error.getField());
+		    assertNotNull(error.getDefaultMessage());
+		    assertEquals("2444", error.getRejectedValue());
+		    assertTrue(error.getCodes().length > 0);
+			assertEquals(BAD_REQUEST, e.getStatusCode());
+			return;
+		}
+		fail("Should throw exception");
+	}
+	
+	@Test
+	public void getOrder() {
+		Order order = new Order("super customer");
+		order.setEmail("customer@example.com");
+		Order response = restTemplate.postForObject(SERVER
+				+ "/orders", order, Order.class);
+
+		Order result = restTemplate.getForObject(SERVER + "/orders/"
+				+ response.getId(), Order.class);
+
+//		assertTrue(hasText(result.getRemoteAddr()));
+		assertEquals("super customer", result.getCustomer());
+		assertEquals("customer@example.com", result.getEmail());
+		assertNotNull(result.getDate());
+		assertNotNull(result.getId());
+		assertEquals(response.getId(), result.getId());
+	}
+}
---
+++ src/rest-test/java/com/gmail/at/zhuikov/aleksandr/rest/xml/XmlRestIT.java
@@ -0,0 +1,188 @@
+package com.gmail.at.zhuikov.aleksandr.rest.xml;
+
+import static junit.framework.Assert.assertEquals;
+import static org.apache.http.auth.AuthScope.ANY_REALM;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.springframework.data.domain.Sort.Direction.DESC;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.validation.FieldError;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+import org.xml.sax.SAXException;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.gmail.at.zhuikov.aleksandr.rest.xml.MyResponseErrorHandler.MyHttpStatusCodeException;
+import com.gmail.at.zhuikov.aleksandr.root.domain.Item;
+import com.gmail.at.zhuikov.aleksandr.root.domain.Order;
+import com.gmail.at.zhuikov.aleksandr.root.domain.xml.XmlFriendlyErrors;
+import com.gmail.at.zhuikov.aleksandr.root.domain.xml.XmlFriendlyPage;
+
+public class XmlRestIT {
+
+	private static final Logger LOG = LoggerFactory.getLogger(XmlRestIT.class);
+	private static final String SERVER = "http://localhost:8080/spring-reference";
+	
+	private RestTemplate restTemplate;
+	
+	@Before
+	public void createRestTemplate() {
+		restTemplate = new RestTemplate(createHttpClientFactory("localhost", 8080));
+		List<HttpMessageConverter<?>> converters = new ArrayList<HttpMessageConverter<?>>();
+		converters.add(new StringHttpMessageConverter());
+		converters.add(new MarshallingHttpMessageConverter(new Jaxb2Marshaller() {{
+			setClassesToBeBound(XmlFriendlyErrors.class, Order.class, XmlFriendlyPage.class);
+		}}));
+		restTemplate.setMessageConverters(converters);
+	}
+	
+	private HttpComponentsClientHttpRequestFactory createHttpClientFactory(
+			String serverName, int serverPort) {
+
+		DefaultHttpClient client = new DefaultHttpClient();
+		client.getCredentialsProvider().setCredentials(
+		        new AuthScope(serverName, serverPort, ANY_REALM), 
+		        new UsernamePasswordCredentials("restuser", "ohW559f5"));
+
+		return new HttpComponentsClientHttpRequestFactory(client);
+	}
+	
+	@Test
+	public void listOrders() throws ParserConfigurationException, SAXException, IOException {
+		String result = restTemplate.getForObject(
+				SERVER + "/orders",
+				String.class);
+		
+		LOG.info(result);
+		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		builder.parse(new ByteArrayInputStream(result.getBytes()));
+	}
+	
+	@Test
+	public void listOrdersReturnsPage() throws JsonParseException, IOException {
+		Order order = new Order("super customer");
+		order.setEmail("customer@example.com");
+		restTemplate.postForLocation(SERVER + "/orders", order);
+		Page<Order> result = restTemplate.getForObject(SERVER + "/orders", XmlFriendlyPage.class);
+		LOG.info(result.toString());
+		assertEquals(0, result.getNumber());
+		assertEquals(20, result.getSize());
+		assertFalse(result.getContent().isEmpty());
+		assertTrue(result.getNumberOfElements() > 0);
+		assertTrue(result.getTotalElements() >= 1);
+		assertTrue(result.getTotalPages() >= 1);
+		assertNotNull(result.getSort());
+		assertEquals(DESC, result.getSort().getOrderFor("date").getDirection());
+	}
+	
+	@Test
+	public void createOrder() {
+		Order order = new Order("super customer");
+		order.setEmail("customer@example.com");
+		Order response = restTemplate.postForObject(SERVER + "/orders", order, Order.class);
+		assertNotNull(response);
+		assertEquals(order, response);
+		assertNotNull(response.getId());
+	}
+	
+	@Test
+	public void createOrderWithItem() {
+		Order order = new Order("super customer");
+		order.setEmail("customer@example.com");
+		new Item(order, "x", 1).setQuantity(1);
+		Order response = restTemplate.postForObject(SERVER + "/orders", order, Order.class);
+		assertEquals(order, response);
+		assertNotNull(response.getId());
+	}
+	
+	@Test
+	public void createOrderAndGetLocation() {
+		Order order = new Order("super customer");
+		order.setEmail("customer@example.com");
+		LOG.info(restTemplate.postForLocation(SERVER + "/orders", order).toString());
+	}
+	
+	@Test
+	public void createWrongOrder() throws JsonParseException, JsonMappingException, IOException {
+		Order request = new Order("2444");
+		request.setEmail("customer@example.com");
+		try {
+			restTemplate.postForObject(SERVER + "/orders", request, Order.class);
+		} catch (HttpStatusCodeException e) {
+			LOG.info(e.getResponseBodyAsString());
+			assertEquals(BAD_REQUEST, e.getStatusCode());
+			return;
+		}
+		fail("Should throw exception");
+	}
+	
+	@Test
+	public void createWrongOrderWithCustomErrorHandler() throws JsonParseException, JsonMappingException, IOException {
+		restTemplate.setErrorHandler(new MyResponseErrorHandler(restTemplate.getMessageConverters()));
+		
+		Order request = new Order("2444");
+		request.setEmail("customer@example.com");
+		try {
+			restTemplate.postForObject(SERVER + "/orders", request, Order.class);
+		} catch (MyHttpStatusCodeException e) {
+			assertEquals(request, e.getErrorBody().getTarget());
+			assertFalse(e.getErrorBody().getErrors().isEmpty());
+			assertTrue(e.getErrorBody().getErrors().get(0) instanceof FieldError);
+			FieldError error = (FieldError) e.getErrorBody().getErrors().get(0);
+			assertEquals("order", error.getObjectName());
+			assertEquals("customer", error.getField());
+		    assertNotNull(error.getDefaultMessage());
+		    assertEquals("2444", error.getRejectedValue());
+		    assertTrue(error.getCodes().length > 0);
+			assertEquals(BAD_REQUEST, e.getStatusCode());
+			return;
+		}
+		fail("Should throw exception");
+	}
+	
+	@Test
+	public void getOrder() {
+		Order order = new Order("super customer");
+		order.setEmail("customer@example.com");
+		Order response = restTemplate.postForObject(SERVER
+				+ "/orders", order, Order.class);
+
+		Order result = restTemplate.getForObject(SERVER + "/orders/"
+				+ response.getId(), Order.class);
+
+		assertEquals("super customer", result.getCustomer());
+		assertEquals("customer@example.com", result.getEmail());
+		assertNotNull(result.getDate());
+		assertNotNull(result.getId());
+		assertEquals(response.getId(), result.getId());
+	}
+}
